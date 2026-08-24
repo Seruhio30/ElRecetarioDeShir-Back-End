e7fa5314f12f3d121775de7d5f621dab355ee520
Sergio Herrera <sh0041314@gmail.com>
2026-08-24 15:59:21 -0600

    chore: establish backend foundation

---
+++ src/test/java/com/elrecetariodeshir/backend/ElRecetarioDeShirBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elrecetariodeshir.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElRecetarioDeShirBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
